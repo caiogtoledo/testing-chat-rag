a51497f92e1fac8ee50db6a4cb5bb71e4534119c
caiogtoledo <ctoledo@pieracciani.com.br>
2024-08-14 04:02:03 +0000

    changes to mvp chat consultant pierx

--- pages/4_💬_Consultor_Pierx_AI.py
+++ pages/4_💬_Consultor_Pierx_AI.py
@@ -3,52 +3,92 @@ import utils
 import streamlit as st
 from streaming import StreamHandler
 
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import DocArrayInMemorySearch
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-st.set_page_config(page_title="ChatPDF", page_icon="📄")
-st.header('Chat with your documents (Basic RAG)')
-st.write('Has access to custom documents and can respond to user queries by referring to the content within those documents')
-st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot/blob/master/pages/4_%F0%9F%93%84_chat_with_your_documents.py)')
+st.set_page_config(page_title="PierX AI", page_icon="📄")
+st.image("https://dev.pierxinovacao.com.br/assets/img/logo.svg", width=120)
+st.header('Fale com o Consultor PierX AI')
+st.write('Pergunte o que quiser sobre o edital "Conhecimento Brasil" da FINEP: ')
 
 class CustomDataChatbot:
 
     def __init__(self):
         self.openai_model = utils.configure_openai()
+        self.directory = "tmp"
+        # self.save_files()
+
+    # def save_files(self):
+    #     tmp_dir = "tmp"
+        
+    #     if not os.path.exists(tmp_dir):
+    #         print(f"A pasta '{tmp_dir}' não existe.")
+    #         return
+        
+    #     for file_name in os.listdir(tmp_dir):
+    #         if file_name.endswith(".pdf"):
+    #             file_path = os.path.join(tmp_dir, file_name)
+    #             with open(file_path, 'rb') as file:
+    #                 self.save_file(file)
 
     def save_file(self, file):
         folder = 'tmp'
         if not os.path.exists(folder):
             os.makedirs(folder)
         
-        file_path = f'./{folder}/{file.name}'
-        with open(file_path, 'wb') as f:
-            f.write(file.getvalue())
-        return file_path
+        # file_path = os.path.join(folder, file.name)
+        with open(file.name, 'wb') as f:
+            f.write(file.read())
+        return file.name
 
-    @st.spinner('Analyzing documents..')
+    @st.spinner('Processando...')
     def setup_qa_chain(self, uploaded_files):
+        if len(uploaded_files) <= 0:
+            files = [file_name for file_name in os.listdir(self.directory) if file_name.endswith(".pdf")]
+        else:
+            files = uploaded_files
+
         # Load documents
         docs = []
-        for file in uploaded_files:
-            file_path = self.save_file(file)
+        for file in files:
+            if len(uploaded_files) <= 0:
+                file_path = f"./{self.directory}/{file}"
+            else:
+                file_path = self.save_file(file)
+            print(file_path)
             loader = PyPDFLoader(file_path)
             docs.extend(loader.load())
         
         # Split documents
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=1000,
-            chunk_overlap=200
+            chunk_overlap=200,
+            separators=[
+                "\n\n",
+                "\n",
+                " ",
+                ".",
+                ",",
+                "\u200b",  # Zero-width space
+                "\uff0c",  # Fullwidth comma
+                "\u3001",  # Ideographic comma
+                "\uff0e",  # Fullwidth full stop
+                "\u3002",  # Ideographic full stop
+                "",
+            ],
         )
         splits = text_splitter.split_documents(docs)
 
         # Create embeddings and store in vectordb
-        embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+        # embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+        # embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
         vectordb = DocArrayInMemorySearch.from_documents(splits, embeddings)
 
         # Define retriever
@@ -79,14 +119,15 @@ class CustomDataChatbot:
     def main(self):
 
         # User Inputs
-        uploaded_files = st.sidebar.file_uploader(label='Upload PDF files', type=['pdf'], accept_multiple_files=True)
-        if not uploaded_files:
-            st.error("Please upload PDF documents to continue!")
-            st.stop()
+        # uploaded_files = st.sidebar.file_uploader(label='Upload PDF files', type=['pdf'], accept_multiple_files=True)
+        # if not uploaded_files:
+        #     st.error("Please upload PDF documents to continue!")
+        #     st.stop()
 
-        user_query = st.chat_input(placeholder="Ask me anything!")
+        user_query = st.chat_input(placeholder="O que deseja saber sobre o edital?")
 
-        if uploaded_files and user_query:
+        if user_query:
+            uploaded_files = []
             qa_chain = self.setup_qa_chain(uploaded_files)
 
             utils.display_msg(user_query, 'user')
@@ -104,7 +145,7 @@ class CustomDataChatbot:
                 for idx, doc in enumerate(result['source_documents'],1):
                     filename = os.path.basename(doc.metadata['source'])
                     page_num = doc.metadata['page']
-                    ref_title = f":blue[Reference {idx}: *{filename} - page.{page_num}*]"
+                    ref_title = f":blue[Referência {idx}: *{filename} - pag.{page_num}*]"
                     with st.popover(ref_title):
                         st.caption(doc.page_content)
 
--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import openai
 import random
 import streamlit as st
 from datetime import datetime
+from dotenv import load_dotenv
+
 
 #decorator
 def enable_chat_history(func):
@@ -22,7 +24,7 @@ def enable_chat_history(func):
 
         # to show chat history on ui
         if "messages" not in st.session_state:
-            st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+            st.session_state["messages"] = [{"role": "assistant", "content": "Como posso te ajudar?"}]
         for msg in st.session_state["messages"]:
             st.chat_message(msg["role"]).write(msg["content"])
 
@@ -41,32 +43,38 @@ def display_msg(msg, author):
     st.chat_message(author).write(msg)
 
 def configure_openai():
-    openai_api_key = st.sidebar.text_input(
-        label="OpenAI API Key",
-        type="password",
-        value=st.session_state['OPENAI_API_KEY'] if 'OPENAI_API_KEY' in st.session_state else '',
-        placeholder="sk-..."
-        )
-    if openai_api_key:
-        st.session_state['OPENAI_API_KEY'] = openai_api_key
-        os.environ['OPENAI_API_KEY'] = openai_api_key
-    else:
-        st.error("Please add your OpenAI API key to continue.")
-        st.info("Obtain your key from this link: https://platform.openai.com/account/api-keys")
-        st.stop()
+    load_dotenv()
+    if "OPENAI_API_KEY" not in os.environ:
+        os.environ['OPENAI_API_KEY'] = st.secrets["OPENAI_API_KEY"] or os.getenv(
+        "OPENAI_API_KEY")
 
-    model = "gpt-3.5-turbo"
+    print("JÁ TENHO A KEY:", os.environ['OPENAI_API_KEY'] )
+    # openai_api_key = st.sidebar.text_input(
+    #     label="OpenAI API Key",
+    #     type="password",
+    #     value=st.session_state['OPENAI_API_KEY'] if 'OPENAI_API_KEY' in st.session_state else '',
+    #     placeholder="sk-..."
+    #     )
+    # if openai_api_key:
+    #     st.session_state['OPENAI_API_KEY'] = openai_api_key
+    #     os.environ['OPENAI_API_KEY'] = openai_api_key
+    # else:
+    #     st.error("Please add your OpenAI API key to continue.")
+    #     st.info("Obtain your key from this link: https://platform.openai.com/account/api-keys")
+    #     st.stop()
+
+    model = "gpt-4o"
     try:
-        client = openai.OpenAI()
-        available_models = [{"id": i.id, "created":datetime.fromtimestamp(i.created)} for i in client.models.list() if str(i.id).startswith("gpt")]
-        available_models = sorted(available_models, key=lambda x: x["created"])
-        available_models = [i["id"] for i in available_models]
+        # client = openai.OpenAI()
+        # available_models = [{"id": i.id, "created":datetime.fromtimestamp(i.created)} for i in client.models.list() if str(i.id).startswith("gpt")]
+        # available_models = sorted(available_models, key=lambda x: x["created"])
+        # available_models = [i["id"] for i in available_models]
 
-        model = st.sidebar.selectbox(
-            label="Model",
-            options=available_models,
-            index=available_models.index(st.session_state['OPENAI_MODEL']) if 'OPENAI_MODEL' in st.session_state else 0
-        )
+        # model = st.sidebar.selectbox(
+        #     label="Model",
+        #     options=available_models,
+        #     index=available_models.index(st.session_state['OPENAI_MODEL']) if 'OPENAI_MODEL' in st.session_state else 0
+        # )
         st.session_state['OPENAI_MODEL'] = model
     except openai.AuthenticationError as e:
         st.error(e.body["message"])
